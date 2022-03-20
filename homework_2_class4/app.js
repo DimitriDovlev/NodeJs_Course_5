8e922de6a9882adbbb0c081f93a2f6e4c505b1f3
DimitriDovlev <dimitri.dovlev2000@gmail.com>
2022-03-20 20:45:29 +0100

    added db.json

--- homework_2_class4/app.js
+++ homework_2_class4/app.js
@@ -1,15 +1,17 @@
-// import { JsonDB } from 'node-json-db';
-// import { Config } from 'node-json-db/dist/lib/JsonDBConfig'
-
-//
-
-const jsonDB = require("node-json-db");
-const config = require("node-json-db/dist/lib/JsonDBConfig");
-
 const http = require("http");
+const dbJson = require("./db.json")
 
 const server = http.createServer((req, res) => {
-    // 
-    console.log(req)
+
+    // console.log(dbJson.clients[0])
+
+    if (req.url == "/") {
+        res.setHeader("Content-Type", "text/html");
+        return res.end(
+            `<p>id: ${dbJson.clients[0].id}</P>
+            <p>name: ${dbJson.clients[0].name}</P>
+            <p>age: ${dbJson.clients[0].age}</P>`
+        );
+    }
 
 }).listen(3000)
