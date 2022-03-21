5e6f4dc355f5c0c8bd65ced687c3c7ea34e590b6
DimitriDovlev <dimitri.dovlev2000@gmail.com>
2022-03-21 10:19:45 +0100

    updated the async version, added sync version

--- homework_1_class3/app.js
+++ homework_1_class3/app.js
@@ -9,20 +9,33 @@ const currPath = __dirname;
 console.log(currPath)
 
 //ime.tip , content vnatre vo file, callback function
-fs.writeFile("homework.txt", "Hello from our first Node homework", function (error) {
+fs.writeFile("homework.txt", "Hello from our first Node homework ", function (error) {
+  if (error) throw error;
+  fs.appendFile("homework.txt", "FINISHED! Async version", function (error) {
     if (error) throw error;
-    console.log("File created sucessfully")
-})
-
-fs.appendFile("homework.txt", "FINISHED", function (error) {
-    if (error) throw error;
+    fs.readFile("homework.txt", "utf8", (err, data) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      console.log(data)
+    })
     console.log("FINISHED!")
+  })
+  console.log("File created sucessfully")
 })
 
-fs.readFile("homework.txt", "utf8" , (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    console.log(data)
-  })
+fs.writeFileSync(path.join(currPath, "homework.txt"), "Hello from our first Node homework ")
+console.log("right after first write")
+
+let data = fs.readFileSync(path.join(currPath, "homework.txt"), "utf-8")
+console.log(data)
+
+fs.appendFileSync(path.join(currPath, "homework.txt"), "Finished! Sync version")
+console.log("after first append")
+
+data = fs.readFileSync(path.join(currPath, "homework.txt"), "utf-8")
+console.log(data)
+
+data = fs.readFileSync(path.join(currPath, "homework.txt"), "utf-8")
+console.log(data)
