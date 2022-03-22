965124746c6e9c87bf79189e7c558e602ec90706
DimitriDovlev <dimitri.dovlev2000@gmail.com>
2022-03-22 17:10:29 +0100

    test

--- homework_2_class4/routes/users.js
+++ homework_2_class4/routes/users.js
@@ -2,8 +2,18 @@ const express = require("express");
 const router = express.Router();
 const dbJson = require("../db.json")
 
-router.get("users", (req, res, next) => {
-
+router.get("/users", (req, res, next) => {
+    const query = req.query;
+    console.log("Query: ",query);
+    // let users = [
+    //   { id: "1", name: "car" },
+    //   { id: "2", name: "chips" },
+    //   { id: "3", name: "laptop" },
+    //   { id: "4", name: "car" },
+    // ];
+    // res.send(users);
 })
+
+
 
 module.exports = router;
