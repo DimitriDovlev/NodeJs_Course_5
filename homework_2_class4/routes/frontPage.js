2df983d5820e40aa2c1d9752f0010837298c9c01
DimitriDovlev <dimitri.dovlev2000@gmail.com>
2022-03-22 17:02:13 +0100

    with express

---
+++ homework_2_class4/routes/frontPage.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const router = express.Router();
+const dbJson = require("../db.json")
+// console.log("ROUTER: ",router)
+router.get("/", (req, res, next) => {
+    res.send(`<form action='/users' method='POST'>Name:<input type='text' name='name'/>age:<input type='text' name='age'/><button type='submit'>SUBMIT</button> </form>
+    <p>id: ${dbJson.clients[0].id}</P>
+    <p>name: ${dbJson.clients[0].name}</P>
+    <p>age: ${dbJson.clients[0].age}</P>`)
+})
+
+module.exports = router;
---
+++ homework_2_class4/routes/users.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const dbJson = require("../db.json")
+
+router.get("users", (req, res, next) => {
+
+})
+
+module.exports = router;
